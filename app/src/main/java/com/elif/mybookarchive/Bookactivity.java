71252e73491a9c5e57f13b28e3e3cff4dac93164
elif <eelifaakay@gmail.com>
2023-04-27 17:34:24 +0300

    permission check

--- app/src/main/java/com/elif/mybookarchive/Bookactivity.java
+++ app/src/main/java/com/elif/mybookarchive/Bookactivity.java
@@ -1,11 +1,16 @@
 package com.elif.mybookarchive;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.elif.mybookarchive.databinding.ActivityBookactivityBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Bookactivity extends AppCompatActivity {
 private ActivityBookactivityBinding binding;
@@ -23,5 +28,23 @@ private ActivityBookactivityBinding binding;
     }
 
     public void selectImage(View view) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE))
+            {
+                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //request permission
+
+                    }
+                }).show();
+            }
+            //request permission
+        }
+        else
+        {
+            //gallery
+        }
     }
 }
