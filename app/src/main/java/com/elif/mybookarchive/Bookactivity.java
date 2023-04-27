9af9115855dd1df358548afae954d0c38d24935f
elif <eelifaakay@gmail.com>
2023-04-27 17:06:31 +0300

    create ui and menu

--- app/src/main/java/com/elif/mybookarchive/Bookactivity.java
+++ app/src/main/java/com/elif/mybookarchive/Bookactivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.elif.mybookarchive.databinding.ActivityBookactivityBinding;
+
 public class Bookactivity extends AppCompatActivity {
+private ActivityBookactivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookactivity);
+        binding=ActivityBookactivityBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
     }
 
     public void save(View view) {
--- app/src/main/java/com/elif/mybookarchive/MainActivity.java
+++ app/src/main/java/com/elif/mybookarchive/MainActivity.java
@@ -1,14 +1,50 @@
 package com.elif.mybookarchive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.elif.mybookarchive.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view =binding.getRoot();
+        setContentView(view);
+    }
+
+    //burada menu koda bağlanır
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater =getMenuInflater();
+        menuInflater.inflate(R.menu.book_menu,menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //tıklandığında ne olacağı
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.add_book)
+        {
+            Intent intent =new Intent(MainActivity.this,Bookactivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
