cb55502762a50b121641c2f5a6783871018c6359
elif <eelifaakay@gmail.com>
2023-04-27 16:00:35 +0300

    create book archive

---
+++ app/src/main/java/com/elif/mybookarchive/Bookactivity.java
@@ -0,0 +1,21 @@
+package com.elif.mybookarchive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Bookactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookactivity);
+    }
+
+    public void save(View view) {
+    }
+
+    public void selectImage(View view) {
+    }
+}
